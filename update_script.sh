38332ef1b086728dd0c371777f2bf80d28c377be
Lukas Bubeliny <lukasbubeliny@gmail.com>
2023-02-27 13:08:53 +0000

    added third version of update_script

--- update_script.sh
+++ update_script.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 RELEASE_FILE=/etc/os-release
+LOGFILE=/var/log/updater.log
+ERRORLOG=/var/log/updater_errors.log
 
 if grep -q "Arch" $RELEASE_FILE
 then
    # The host is based on Arch, run the pacman update command
-   sudo pacman -Syu
+   sudo pacman -Syu 1>>$LOGFILE 2>>$ERRORLOG
+   if [ $? -ne 0 ]
+   then
+      echo "An error occured, please check the $ERRORLOG file."
+   fi      
 fi
 
 if grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE
 then
    # The host is based on Debian or Ubuntu, run the apt version of the command"
-   sudo apt update
-   sudo apt dist-upgrade
+   sudo apt update -y 1>>$LOGFILE 2>>$ERRORLOG
+   if [ $? -ne 0 ]
+   then
+      echo "An error occured, please check the $ERRORLOG file."
+   fi
+   sudo apt dist-upgrade -y  1>>$LOGFILE 2>>$ERRORLOG
+   if [ $? -ne 0 ]
+   then
+      echo "An error occured, please check the $ERRORLOG file."
+   fi
 fi
 
 
