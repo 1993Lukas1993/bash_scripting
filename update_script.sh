3b2b8deab0f048ae73b5e93f167dfe042c5b1b1f
Lukas Bubeliny <lukasbubeliny@gmail.com>
2023-02-27 15:32:49 +0000

    added functions to update script

--- update_script.sh
+++ update_script.sh
@@ -4,29 +4,30 @@ RELEASE_FILE=/etc/os-release
 LOGFILE=/var/log/updater.log
 ERRORLOG=/var/log/updater_errors.log
 
+check_exit_status() {
+    if [ $? -ne 0 ]
+    then
+        echo "An error occured, please check the $ERRORLOG file."
+    fi      
+}
+
+
+
 if grep -q "Arch" $RELEASE_FILE
 then
    # The host is based on Arch, run the pacman update command
    sudo pacman -Syu 1>>$LOGFILE 2>>$ERRORLOG
-   if [ $? -ne 0 ]
-   then
-      echo "An error occured, please check the $ERRORLOG file."
-   fi      
+   check_exit_status
 fi
 
 if grep -q "Debian" $RELEASE_FILE || grep -q "Ubuntu" $RELEASE_FILE
 then
    # The host is based on Debian or Ubuntu, run the apt version of the command"
    sudo apt update -y 1>>$LOGFILE 2>>$ERRORLOG
-   if [ $? -ne 0 ]
-   then
-      echo "An error occured, please check the $ERRORLOG file."
-   fi
+   check_exit_status
+
    sudo apt dist-upgrade -y  1>>$LOGFILE 2>>$ERRORLOG
-   if [ $? -ne 0 ]
-   then
-      echo "An error occured, please check the $ERRORLOG file."
-   fi
+   check_exit_status
 fi
 
 
